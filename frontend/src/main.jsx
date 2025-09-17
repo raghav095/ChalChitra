48890b2e194992844729c0ef986da4c582cbcd50
Raghav Rathi <raghavrathi5555@gmail.com>
2025-09-17 12:04:22 +0530

    web analytics insrtalled

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Import Vercel Analytics
+import { Analytics } from '@vercel/analytics/react'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics /> {/* Add this for Vercel Analytics */}
   </StrictMode>,
 )
