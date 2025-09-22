c0584655e647c6106ca3b16f4584ccc7033d783f
Raghav Rathi <raghavrathi5555@gmail.com>
2025-09-22 19:12:02 +0530

    axios.js fixed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,9 @@
-// src/api/axios.js
-
 import axios from 'axios';
 
-// Get the backend URL from the environment variables
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
-export default axios.create({
-  baseURL: BACKEND_URL,
-  withCredentials: true // This is the magic line!
+const api = axios.create({
+  // This will use your Vercel variable in production and your proxy in development
+  baseURL: import.meta.env.VITE_BACKEND_URL || '', 
+  withCredentials: true,
 });
+
+export default api;
