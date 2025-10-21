5756d45339513507451106d645909b1a778b4945
Raghav Rathi <raghavrathi5555@gmail.com>
2025-10-21 14:39:26 +0530

    updated routes for prifle google

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { userregistration , usersignin, logoutuser,refreshaccesstoekn}  from "../controllers/user.controller.js";
 import { verify } from "../middlewares/auth.middleware.js";
 import passport from "passport";
+import { Apierror } from "../utils/Apierrors.js";
 const router = Router();
 
 router.route("/register").post(userregistration)
@@ -21,6 +22,17 @@ router.get(
   }
 );
 
+router.get("/me" , verify,async(req,res)=>{
+    try {
+     if (!req.user) {
+      throw new Apierror(401 , "Not Authenticated")
+    }
+    res.json({ user: req.user });
+        
+    } catch (error) {
+       throw new Apierror(500 , "server error ")
+    }
+})
 router.route("/logout").post(verify,logoutuser)
 router.route("/refrshed-token").post(refreshaccesstoekn)
 
--- frontend/src/pages/Mainpage.jsx
+++ frontend/src/pages/Mainpage.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { use, useEffect } from "react";
 import Navbarmain from "../components/Navbarmain";
 import MovieRow from "../components/MovieRow.jsx";
+import { useDispatch } from "react-redux";
+import {login} from "../features/userSlice.js"
+
 
 const Mainpage = () => {
+
+     const dispatch = useDispatch();
+
+    useEffect(() => {
+
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/users/me`, {
+      credentials: "include", 
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.user) {
+          dispatch(login(data.user));
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("isAuthenticated", "true");
+        }
+      })
+      .catch((err) => {
+        
+        console.error("Failed to fetch user info:", err);
+      });
+  }, [dispatch]);
+
+
+
   return (
     <>
       <Navbarmain />
